b1b328cd92646ed6a1ce7c65b584d32f51c996d1
Jelmer Cormont <jelmer@cormont.work>
2025-05-14 22:48:41 +0800

    Pages: use correct image URL with vite import

--- playground/activities/layouts/links.ts
+++ playground/activities/layouts/links.ts
@@ -1,85 +1,102 @@
+import navBarOnlySvg from "./images/navbar-only.svg";
+import darkNavbarSvg from "./images/dark-navbar.svg";
+import dynamicNavbarSvg from "./images/dynamic-navbar.svg";
+import navbarOverlapSvg from "./images/navbar-overlap.svg";
+import stackedHeadingSvg from "./images/stacked-heading.svg";
+import stackedHeadingWellSvg from "./images/stacked-heading-well.svg";
+import sidebarOnlySvg from "./images/sidebar-only.svg";
+import darkSidebarSvg from "./images/dark-sidebar.svg";
+import navbarSidebarFixedSvg from "./images/navbar-sidebar-fixed.svg";
+import navbarSidebarSubSvg from "./images/navbar-sidebar-sub.svg";
+import sidebarCompactSvg from "./images/sidebar-compact.svg";
+import sidebarDrawerSvg from "./images/sidebar-drawer.svg";
+import authSimpleSvg from "./images/auth-simple.svg";
+import authCardSvg from "./images/auth-card.svg";
+import authImageSvg from "./images/auth-image.svg";
+import pageFormSvg from "./images/page-form.svg";
+
 export default {
 	layouts: [
 		{
 			navigateTo: "navbar-only",
-			image: "/images/navbar-only.svg",
+			image: navBarOnlySvg,
 			title: "Navbar only",
 		},
 		{
 			navigateTo: "dark-navbar",
-			image: "/images/dark-navbar.svg",
+			image: darkNavbarSvg,
 			title: "Dark navbar",
 		},
 		{
 			navigateTo: "dynamic-navbar",
-			image: "/images/dynamic-navbar.svg",
+			image: dynamicNavbarSvg,
 			title: "Dynamic navbar",
 		},
 		{
 			navigateTo: "navbar-overlap",
-			image: "/images/navbar-overlap.svg",
+			image: navbarOverlapSvg,
 			title: "Navbar with overlap",
 		},
 		{
 			navigateTo: "stacked-heading",
-			image: "/images/stacked-heading.svg",
+			image: stackedHeadingSvg,
 			title: "Stacked heading",
 		},
 		{
 			navigateTo: "stacked-heading-well",
-			image: "/images/stacked-heading-well.svg",
+			image: stackedHeadingWellSvg,
 			title: "Stacked heading with well",
 		},
 		{
 			navigateTo: "sidebar-only",
-			image: "/images/sidebar-only.svg",
+			image: sidebarOnlySvg,
 			title: "Sidebar only",
 		},
 		{
 			navigateTo: "dark-sidebar",
-			image: "/images/dark-sidebar.svg",
+			image: darkSidebarSvg,
 			title: "Dark sidebar",
 		},
 		{
 			navigateTo: "navbar-sidebar-fixed",
-			image: "/images/navbar-sidebar-fixed.svg",
+			image: navbarSidebarFixedSvg,
 			title: "Navbar with fixed sidebar",
 		},
 		{
 			navigateTo: "navbar-sidebar-sub",
-			image: "/images/navbar-sidebar-sub.svg",
+			image: navbarSidebarSubSvg,
 			title: "Navbar with sidebar below",
 		},
 		{
 			navigateTo: "sidebar-compact",
-			image: "/images/sidebar-compact.svg",
+			image: sidebarCompactSvg,
 			title: "Sidebar (compact)",
 		},
 		{
 			navigateTo: "sidebar-drawer",
-			image: "/images/sidebar-drawer.svg",
+			image: sidebarDrawerSvg,
 			title: "Sidebar with drawer",
 		},
 	],
 	pages: [
 		{
 			navigateTo: "auth-simple",
-			image: "/images/auth-simple.svg",
+			image: authSimpleSvg,
 			title: "Simple login",
 		},
 		{
 			navigateTo: "auth-card",
-			image: "/images/auth-card.svg",
+			image: authCardSvg,
 			title: "Login (with card)",
 		},
 		{
 			navigateTo: "auth-image",
-			image: "/images/auth-image.svg",
+			image: authImageSvg,
 			title: "Login (with image)",
 		},
 		{
 			navigateTo: "page-form",
-			image: "/images/page-form.svg",
+			image: pageFormSvg,
 			title: "Form page",
 		},
 	],
